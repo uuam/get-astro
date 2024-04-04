545cdc2e6ded8d4bf36e507b6891905f54bce6a4
uuam <chen552025@gmail.com>
2024-04-04 17:53:43 +0800

    reslove greeting variable can't random

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://get-astro.vercel.app/",
-  integrations: [react(), tailwind()],
+  integrations: [tailwind(), preact()],
   output: "server",
   adapter: vercel()
 });
