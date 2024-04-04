b77b50189b4c15c846614119314b198f68c374a3
uuam <chen552025@gmail.com>
2024-04-04 15:32:13 +0800

    change site value

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://get-astro.vercel.app/",
   integrations: [react(), tailwind()],
   output: "server",
   adapter: vercel()
